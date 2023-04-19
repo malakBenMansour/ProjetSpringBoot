502692de3438633e231971b5256d9da9f2300f9e
malak_ben_mansour <61378747+malakBenMansour@users.noreply.github.com>
2023-04-20 00:04:07 +0100

    Delete pi directory

--- pi/src/main/java/arctic/example/pi/repository/EventRepository.java
+++
@@ -1,9 +0,0 @@
-package tn.esprit.picloudbeta.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import tn.esprit.picloudbeta.entity.Evenement;
-
-@Repository
-public interface EventRepository extends CrudRepository<Evenement,Long> {
-}
--- pi/src/main/java/arctic/example/pi/repository/SponsorRepository.java
+++
@@ -1,9 +0,0 @@
-package tn.esprit.picloudbeta.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import tn.esprit.picloudbeta.entity.Sponsor;
-
-@Repository
-public interface SponsorRepository extends CrudRepository<Sponsor,Long> {
-}
