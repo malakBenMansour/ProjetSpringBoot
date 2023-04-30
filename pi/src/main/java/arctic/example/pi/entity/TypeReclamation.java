799083182dc171e3b9487d1886e555e77679d4a9
Nour Ben Ali <nour.benali@esprit.com>
2023-04-30 17:09:41 +0200

    métiers avancés

---
+++ pi/src/main/java/arctic/example/pi/entity/TypeReclamation.java
@@ -0,0 +1,75 @@
+package arctic.example.pi.entity;
+
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class TypeReclamation implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long idType ;
+    private String nom;
+    private String description;
+    @Temporal(TemporalType.DATE)
+//    private LocalDateTime dateCreation;
+    @JsonFormat(pattern="yyyy-MM-dd")
+    private Date dateCreation;
+
+
+    @OneToMany(mappedBy = "typeReclamation", cascade = CascadeType.ALL)
+
+    private List<Reclamation> reclamations;
+
+    public long getIdType() {
+        return idType;
+    }
+
+    public void setIdType(long idType) {
+        this.idType = idType;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDateCreation() {
+        return dateCreation;
+    }
+
+    public void setDateCreation(Date dateCreation) {
+        this.dateCreation = dateCreation;
+    }
+
+    public List<Reclamation> getReclamations() {
+        return reclamations;
+    }
+
+    public void setReclamations(List<Reclamation> reclamations) {
+        this.reclamations = reclamations;
+    }
+}
---
+++ pi/src/main/java/arctic/example/pi/service/TypeRecService.java
@@ -0,0 +1,69 @@
+package arctic.example.pi.service;
+
+
+import arctic.example.pi.DTO.CountType;
+import arctic.example.pi.repository.TypeRecRepository;
+import arctic.example.pi.entity.TypeReclamation;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+
+@AllArgsConstructor
+@Service
+public class TypeRecService implements ITypeRec{
+
+    @Autowired
+    TypeRecRepository typeRecRepository;
+
+    public TypeReclamation addTypeReclamation(TypeReclamation t) {
+        return typeRecRepository.save(t);
+    }
+    public void deleteTypeRec(Long idType) {
+        typeRecRepository.deleteById(idType);
+
+    }
+
+
+    public TypeReclamation updateTypeReclamation(TypeReclamation t) {
+        return typeRecRepository.save(t);
+    }
+
+    public List<TypeReclamation> retrieveAllTypeReclamations() {
+        List<TypeReclamation> tr = (List<TypeReclamation>) typeRecRepository.findAll();
+        return tr;
+    }
+
+
+    public TypeReclamation retrieveTypeReclamation(Long  idType) {
+        return typeRecRepository.findById(idType).orElse(null);
+    }
+
+    public Set<TypeReclamation> findTypeByDateCreation(Date DateCreation) {
+        return typeRecRepository.findTypeByDateCreation(DateCreation);
+    }
+
+    public int countAllByNom(String nom) {
+        return typeRecRepository.countAllByNom(nom);
+    }
+    public List<TypeReclamation> findTypeReclamationByDateCreationBetween(Date date1, Date date2) {
+        return typeRecRepository.findTypeReclamationByDateCreationBetween( date1,date2);
+    }
+
+    public List<CountType> statistque()
+    {
+        return typeRecRepository.statistque();
+    }
+
+//me tekhdmsh
+    public List<Object[]> nbReclamationBytypeBetweenDeuxDates(Date date1, Date date2) {
+        return typeRecRepository.nbReclamationBytypeBetweenDeuxDates(date1,date2);
+    }
+
+
+
+
+}
